a85b9f40d26f82e786e4d6d23bedab80ae8aeade
Krzysztof Pajak <krzysiek@grandnode.com>
2023-03-10 19:55:59 +0100

    Update GrandVersion.cs

--- src/Core/Grand.Infrastructure/GrandVersion.cs
+++ src/Core/Grand.Infrastructure/GrandVersion.cs
@@ -1,8 +1,9 @@
-﻿namespace Grand.Infrastructure
+using System.Reflection;
+
+namespace Grand.Infrastructure
 {
     public static class GrandVersion
     {
-
         /// <summary>
         /// Gets the major version
         /// </summary>
@@ -36,11 +37,31 @@
         /// <summary>
         /// Gets the git branch
         /// </summary>
-        public const string GitBranch = ThisAssembly.Git.Branch;
-        
+        public const string GitBranch = "";
+
         /// <summary>
         /// Gets the git commit
         /// </summary>
-        public const string GitCommit = ThisAssembly.Git.Commit;
+        public static string GitCommit = GetGitHash();
+        
+        private static string GetGitHash()
+        {
+            if (!string.IsNullOrEmpty(_gitHash)) return _gitHash;
+            
+            var version = "1.0.0+LOCALBUILD"; // Dummy version for local dev
+            var appAssembly = typeof(GrandVersion).Assembly;
+            var infoVerAttr = (AssemblyInformationalVersionAttribute)appAssembly
+                .GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute)).FirstOrDefault();
+            if (infoVerAttr is { InformationalVersion.Length: > 6 })
+            {
+                // Hash is embedded in the version after a '+' symbol, e.g. 1.0.0+a34a913742f8845d3da5309b7b17242222d41a21
+                version = infoVerAttr.InformationalVersion;
+            }
+            _gitHash = version[(version.IndexOf('+') + 1)..];
+
+            return _gitHash;
+        }
+
+        private static string _gitHash { get; set; }
     }
 }
